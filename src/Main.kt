16473f12e88206274d829be7b0b783518b5c8f27
Ugo119 <onuoha.ugochukwu@yahoo.com>
2020-10-05 11:40:13 +0100

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,28 @@
+fun main() {
+    println("Pow!")
+    var name = "Ade"
+//    println(name)
+//    var x = 1
+//    while (x < 15) {
+//      x += 1
+//      println("x is now $x.")
+//    }
+
+//    for (i in 1..10) {
+//        x += 1
+//        println("x is now $x.")
+//    }
+
+//    var x = 12
+//    var y =38
+//    print(if (x > y) "x is greater than y" else "x is not greater than y")
+    //Assign a variable to another of different type
+//    var x = 209
+//    var y : Long = x.toLong()
+//    print(y)
+
+    //Create an array
+    var myArray = arrayOf(1,2,3)
+    print(myArray[1])
+
+}
